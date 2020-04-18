7f86aadb4f94a27b79909330cbff519e032e8424
Viktoria Radchenko <viktoriia.rad@gmail.com>
2020-04-18 16:09:01 +0200

    Add protocol checking

--- build/app.js
+++ build/app.js
@@ -1,3 +1,6 @@
 import { Game } from './game/index.js';
+if (window.location.protocol !== 'https:') {
+    window.location.href = 'https:' + window.location.href.substring(window.location.protocol.length);
+}
 const game = new Game(1.5);
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,7 @@
-import { Game } from './game/index.js'
+import { Game } from './game/index.js';
+
+if (window.location.protocol !== 'https:') {
+  window.location.href = 'https:' + window.location.href.substring(window.location.protocol.length);
+}
 
 const game = new Game(1.5);
